6b166a233ac0b2bee6528bdddfb8815705c6cd3a
Kevin L. Mitchell <klmitch@mit.edu>
2013-10-06 09:22:45 -0500

    Skeleton of policies.

---
+++ README.rst
@@ -0,0 +1,5 @@
+========
+Policies
+========
+
+This is a package for evaluating access control policies.
---
+++ setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import os
+
+from setuptools import setup
+
+
+def readreq(filename):
+    result = []
+    with open(filename) as f:
+        for req in f:
+            req = req.lstrip()
+            if req.startswith('-e ') or req.startswith('http:'):
+                idx = req.find('#egg=')
+                if idx >= 0:
+                    req = req[idx + 5:].partition('#')[0].strip()
+                else:
+                    pass
+            else:
+                req = req.partition('#')[0].strip()
+            if not req:
+                continue
+            result.append(req)
+    return result
+
+
+def readfile(filename):
+    with open(filename) as f:
+        return f.read()
+
+
+setup(
+    name='policies',
+    version='0.1',
+    author='Kevin L. Mitchell',
+    author_email='klmitch@mit.edu',
+    url='https://github.com/klmitch/policies',
+    description="An access policy language evaluator.",
+    long_description=readfile('README.rst'),
+    license='GNU General Public License v3 or later (GPLv3+)',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 '
+            'or later (GPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries',
+    ],
+    packages=['policies'],
+    install_requires=readreq('requirements.txt'),
+    tests_require=readreq('test-requirements.txt'),
+    entry_points={
+    },
+)
