68734472a4c79779bca34435a0dcabef57dcc24f
Kevin L. Mitchell <klmitch@mit.edu>
2013-10-06 15:56:06 -0500

    Add a base TestCase class.

--- tests/__init__.py
+++ tests/__init__.py
@@ -13,3 +13,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see
 # <http://www.gnu.org/licenses/>.
+
+import unittest2
+
+
+class TestCase(unittest2.TestCase):
+    pass
