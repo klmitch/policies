56d2eeb7823fb12390d4f6677178cf17416009db
Kevin L. Mitchell <klmitch@mit.edu>
2013-10-09 22:46:47 -0500

    Create, document, and test Authorization
    The Authorization object is the results of an authorization check.  It
    is constructed by the policies.instructions.set_authz instruction, and
    updated by the policies.instructions.AuthorizationAttr instructions.
    It has a boolean value indicating whether authorization succeeded or
    not, and allows arbitrary authorization attributes to be queried
    (defaulting to None if the value hasn't been set to a default on the
    rule or by a parsed authorization rule).

---
+++ policies/authorization.py
@@ -0,0 +1,92 @@
+# Copyright (C) 2013 by Kevin L. Mitchell <klmitch@mit.edu>
+#
+# This program is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see
+# <http://www.gnu.org/licenses/>.
+
+
+class Authorization(object):
+    """
+    The authorization result.  Evaluates as either ``True`` or
+    ``False``, depending on what the result of the policy evaluation
+    was.  Can also have attributes set by the policy rule, which will
+    default to ``None`` unless another default is provided.
+    """
+
+    def __init__(self, result, defaults):
+        """
+        Initialize an ``Authorization`` object.
+
+        :param result: The result of the authorization evaluation.
+                       Will be converted to boolean using the
+                       ``bool()`` function.
+        :param defaults: A dictionary of default values for the
+                         authorization attributes.  If not provided,
+                         authorization attributes default to ``None``.
+        """
+
+        super(Authorization, self).__setattr__('_result', bool(result))
+        super(Authorization, self).__setattr__('_attrs', defaults.copy())
+
+    def __getattr__(self, name):
+        """
+        Retrieve a named authorization attribute.  If the
+        authorization attribute has not been set, returns ``None``.
+
+        :param name: The name of the authorization attribute to
+                     return.
+
+        :returns: The value of the authorization attribute.
+        """
+
+        # Prohibit access to internal attributes, so users don't get
+        # any ideas...
+        if name[0] == '_':
+            raise AttributeError("%r object has no attribute %r" %
+                                 (self.__class__.__name__, name))
+
+        return self._attrs.get(name)
+
+    def __setattr__(self, name, value):
+        """
+        Prohibits setting authorization attributes.  Authorization
+        attributes are immutable.
+
+        :param name: The name of the authorization attribute to set.
+        :param value: The value to set the authorization attribute to.
+        """
+
+        raise AttributeError("%r object does not allow setting attribute %r" %
+                             (self.__class__.__name__, name))
+
+    def __delattr__(self, name):
+        """
+        Prohibits deletion of authorization attributes.  Authorization
+        attributes are immutable.
+
+        :param name: The name of the authorization attribute to set.
+        """
+
+        raise AttributeError("%r object does not allow deleting attribute %r" %
+                             (self.__class__.__name__, name))
+
+    def __nonzero__(self):
+        """
+        Returns the result of the authorization check.  If the
+        ``result`` passed to the constructor evaluated as ``True``,
+        returns ``True``; otherwise, returns ``False``.
+
+        :returns: The result of the authorization check.
+        """
+
+        return self._result
---
+++ tests/unit/test_authorization.py
@@ -0,0 +1,83 @@
+# Copyright (C) 2013 by Kevin L. Mitchell <klmitch@mit.edu>
+#
+# This program is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see
+# <http://www.gnu.org/licenses/>.
+
+from policies import authorization
+
+import tests
+
+
+class TestAuthorization(tests.TestCase):
+    def test_init_true(self):
+        attrs = {'a': 1, 'b': 2, 'c': 3}
+        authz = authorization.Authorization(1, attrs)
+
+        self.assertEqual(authz._result, True)
+        self.assertEqual(authz._attrs, attrs)
+        self.assertNotEqual(id(authz._attrs), id(attrs))
+
+    def test_init_false(self):
+        attrs = {'a': 1, 'b': 2, 'c': 3}
+        authz = authorization.Authorization('', attrs)
+
+        self.assertEqual(authz._result, False)
+        self.assertEqual(authz._attrs, attrs)
+        self.assertNotEqual(id(authz._attrs), id(attrs))
+
+    def test_init_attrs_indep(self):
+        attrs = {'a': 1, 'b': 2, 'c': 3}
+        authz = authorization.Authorization('', attrs)
+
+        attrs['d'] = 4
+
+        self.assertEqual(authz._attrs, {'a': 1, 'b': 2, 'c': 3})
+
+    def test_getattr(self):
+        attrs = {'a': 1, 'b': 2, 'c': 3}
+        authz = authorization.Authorization(True, attrs)
+
+        self.assertEqual(authz.a, 1)
+        self.assertEqual(authz.b, 2)
+        self.assertEqual(authz.c, 3)
+        self.assertEqual(authz.d, None)
+
+        authz._attrs['d'] = 4
+
+        self.assertEqual(authz.d, 4)
+
+        self.assertRaises(AttributeError, getattr, authz, '_a')
+
+    def test_setattr(self):
+        authz = authorization.Authorization(True, {})
+
+        self.assertRaises(AttributeError, setattr, authz, 'a', 1)
+
+    def test_delattr(self):
+        def del_attr(az):
+            del az.a
+
+        authz = authorization.Authorization(True, {})
+
+        self.assertRaises(AttributeError, delattr, authz, 'a')
+
+    def test_boolean_true(self):
+        authz = authorization.Authorization(True, {})
+
+        self.assertTrue(authz)
+
+    def test_boolean_false(self):
+        authz = authorization.Authorization(False, {})
+
+        self.assertFalse(authz)
