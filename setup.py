8a4f166dc57b9be715a02b7ca972f148336917bf
Kevin L. Mitchell <klmitch@mit.edu>
2013-10-16 19:56:12 -0500

    Bump version.

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ def readfile(filename):
 
 setup(
     name='policies',
-    version='0.3',
+    version='0.3.1',
     author='Kevin L. Mitchell',
     author_email='klmitch@mit.edu',
     url='https://github.com/klmitch/policies',
